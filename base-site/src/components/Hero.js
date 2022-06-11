92fcbc7acb0f9867becf8519134283a39c92c02d
Patrick Bradshaw <patrick.j.bradshaw@gmail.com>
2022-06-10 18:12:48 -0700

    Restarted create-react-app. Don't use SASS. Prefer Tailwind. Add components

---
+++ base-site/src/components/Hero.js
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div className='bg-gray-500 flex items-center justify-center h-3/4'>
+      <h1 className="text-8xl font-thin text-center text-zinc-100 inline-block">Hero</h1>
+    </div>
+  );
+}
+
+export default Hero
